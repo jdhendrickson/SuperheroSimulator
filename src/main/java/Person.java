0da2a57702c54800b8150decb23c43d1174d59b4
jdhendrickson <jdhendr4@asu.edu>
2020-04-08 00:43:12 -0700

    Fixed the location of the getters and setters

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -12,7 +12,63 @@ interface Person {
     boolean takeDamage(int in);
 
     /**
-     * A getter for health
+     * A getter for the current armor.
+     * @return The amount of armor right now.
      */
-    int getCurrentHealth();
+    public int getArmor();
+
+    /**
+     * A setter for the amount of armor.
+     * @param armor The new amount of armor.
+     */
+    public void setArmor(int armor);
+
+    /**
+     * A getter for the current amount of health.
+     * @return The current amount of health.
+     */
+    public int getCurrentHealth();
+
+    /**
+     * A setter for the current amount of health.
+     * WARNING: Does not allow health to go above max.
+     * @param health The new amount of health.
+     */
+    public void setCurrentHealth(int health);
+
+    /**
+     * A getter for the current damage value.
+     * @return The amount of damage.
+     */
+    public int getDamage();
+
+    /**
+     * A setter for the amount of damage,
+     * @param damage The new damage value.
+     */
+    public void setDamage(int damage);
+
+    /**
+     * A getter for the dodge damage reduction.
+     * @return The amount of dodge.
+     */
+    public int getDodge();
+
+    /**
+     * A setter for the amount of dodge.
+     * @param dodge The new amount of dodge.
+     */
+    public void setDodge(int dodge);
+
+    /**
+     * A getter for the max amount of health.
+     * @return The maximum amount of health.
+     */
+    int setMaxHealth();
+
+    /**
+     * A setter for the amount of health.
+     * @param health The new maximum amount of health.
+     */
+    public void setMaxHealth(int health);
 }
--- src/main/java/PowerDecorator.java
+++ src/main/java/PowerDecorator.java
@@ -1,4 +1,4 @@
-public class PowerDecorator extends CapeDecorator{
+public class PowerDecorator extends CapeDecorator {
     /**
      * A cape decorator thing.
      * @param impDam    The new damage value.
@@ -6,7 +6,7 @@ public class PowerDecorator extends CapeDecorator{
      * @param impArm    The new armor value.
      * @param impHealth The new health value.
      */
-    public PowerDecorator(Cape cape, int impDam, int impDodge, int impArm, int impHealth) {
+    public PowerDecorator(Person cape, int impDam, int impDodge, int impArm, int impHealth) {
         super(cape);
         cape.setDamage(impDam);
         cape.setDodge(impDodge);
@@ -26,7 +26,52 @@ public class PowerDecorator extends CapeDecorator{
     }
 
     @Override
+    public int getArmor() {
+        return 0;
+    }
+
+    @Override
+    public void setArmor(int armor) {
+
+    }
+
+    @Override
     public int getCurrentHealth() {
         return 0;
+    }
+
+    @Override
+    public void setCurrentHealth(int health) {
+
+    }
+
+    @Override
+    public int getDamage() {
+        return 0;
+    }
+
+    @Override
+    public void setDamage(int damage) {
+
+    }
+
+    @Override
+    public int getDodge() {
+        return 0;
+    }
+
+    @Override
+    public void setDodge(int dodge) {
+
+    }
+
+    @Override
+    public int setMaxHealth() {
+        return 0;
+    }
+
+    @Override
+    public void setMaxHealth(int health) {
+
     }
 }
