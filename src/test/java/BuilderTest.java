9a1ad7aaddb58581468f7f77c0626d7e84d59610
jdhendrickson <jdhendr4@asu.edu>
2020-04-08 01:34:20 -0700

    Made a basic test class for the hideouts

---
+++ src/test/java/BuilderTest.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BuilderTest {
+    Hideout hideout;
+    /**
+     * Set up the basic Cape class
+     * @throws Exception There was an error with setting up Junit
+     */
+    @org.junit.Before
+    public void setUp() throws Exception {
+        hideout = new UndergroundCaveBuilder().getHideout();
+    }
+
+    //Check to make sure the parameters are correct
+    @Test
+    public void testBasicHideout() {
+        assertEquals(10, hideout.getDurability());
+    }
+}
